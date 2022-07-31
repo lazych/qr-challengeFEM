955164951fc6f999751640a2004e21c80427466d
jack <Ciao@lazych.me>
2022-07-31 20:54:40 +0100

    set-up

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    mode: 'development',
+    devServer: {
+        port: 8080,
+        static: path.resolve(__dirname, 'dist'),
+        hot: true, 
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use : ["style-loader", "css-loader", "postcss-loader"]
+            }
+        ]
+    }, 
+
+    
+}
