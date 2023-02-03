51b455abc061c608733c0c540a2d1564139f1770
jack <Ciao@lazych.me>
2023-02-03 17:05:51 +0100

    ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
-const path = require("path");
+const path = require("path")
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
 
-    mode: 'development',
-    devServer: {
-        port: 8080,
-        static: path.resolve(__dirname, 'dist'),
-        hot: true, 
-    },
+  mode: "development",
+  devServer: {
+    port: 8080,
+    static: path.resolve(__dirname, "dist"),
+    hot: true,
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use : ["style-loader", "css-loader", "postcss-loader"]
-            }
-        ]
-    }, 
-
-    
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
 }
